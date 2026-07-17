facdafe2ab75c8f4c7c49c2346e652750abada48
mattruffolo <mattruffolo99@gmail.com>
2026-07-17 10:08:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,19 +23,27 @@ pop_gap = int(summary.loc[summary["minutes"] == 999, "Population.sum"].values[0]
 c1, c2, c3, c4 = st.columns(4)
 c1.markdown(f"""
 ### 5 Minute Coverage
-**{pop_5:,}**
-<br>
+# {pop_5:,}
 <span style='color:green'>{pop_5/total_pop*100:.1f}% of population</span>
 """, unsafe_allow_html=True)
-c2.metric("10 Minute Coverage", f"{pop_10:,}")
-c3.metric("15 Minute Coverage", f"{pop_15:,}")
-c4.metric("Outside Coverage", f"{pop_gap:,}")
-st.write(
-    f"5 min: {pop_5/total_pop*100:.1f}% | "
-    f"10 min: {pop_10/total_pop*100:.1f}% | "
-    f"15 min: {pop_15/total_pop*100:.1f}% | "
-    f"Outside: {pop_gap/total_pop*100:.1f}%"
-)
+
+c2.markdown(f"""
+### 10 Minute Coverage
+# {pop_10:,}
+<span style='color:green'>{pop_10/total_pop*100:.1f}% of population</span>
+""", unsafe_allow_html=True)
+
+c3.markdown(f"""
+### 15 Minute Coverage
+# {pop_15:,}
+<span style='color:green'>{pop_15/total_pop*100:.1f}% of population</span>
+""", unsafe_allow_html=True)
+
+c4.markdown(f"""
+### Outside Coverage
+# {pop_gap:,}
+<span style='color:green'>{pop_gap/total_pop*100:.1f}% of population</span>
+""", unsafe_allow_html=True)
 
 st.subheader("Coverage Map")
 m = folium.Map(
