c4ef28bac4c6e94a1acd55e969fe0583e56b531f
mattruffolo <mattruffolo99@gmail.com>
2026-07-17 15:00:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ color:black;
 <span style="color:#2ecc71;">■</span> 5 Minute Drive Time<br>
 <span style="color:#f1c40f;">■</span> 10 Minute Drive Time<br>
 <span style="color:#e74c3c;">■</span> 15 Minute Drive Time<br><br>
-<span style="color:#777777;">- - -</span> York Region Boundary<br>
+<span style="color:#777777;">---</span> York Region Boundary<br>
 
 </div>
 """
