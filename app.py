214ba47c144666e9508979fe0b9b3f554735d8ea
mattruffolo <mattruffolo99@gmail.com>
2026-07-23 11:27:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ color:black;
 
 <b>Legend</b><br><br>
 
-<span style="font-size:16px;">⛊✪</span> Police Station<br>
+<span style="font-size:16px;">✪</span> Police Station<br>
 
 <span style="color:#2ecc71;">■</span> 5 Minute Drive Time<br>
 <span style="color:#f1c40f;">■</span> 10 Minute Drive Time<br>
